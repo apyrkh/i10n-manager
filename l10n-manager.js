07ac42625f0c203e46f0d09be17328f6721e05db
Alexander Pyrkh <alexpyrkh@gmail.com>
2019-04-09 02:47:32 +0300

    Implemented middleware concept of l10n manager

--- l10n-manager.js
+++ l10n-manager.js
@@ -1,13 +1,33 @@
-function LocalizationManager(locale = 'en', l10nResources) {
-  const currentLocale = locale;
-  const texts = l10nResources || {};
+function LocalizationManager() {
+  const middlewares = Array.from(arguments);
+  const l10nNames = [];
+  const l10nTexts = {};
+
+  function applyMiddleware(text) {
+    return middlewares.reduce((prevText, middleware, index) => {
+      if (typeof middleware !== 'function') throw new Error('LocalizationManager: middleware [' + index + '] must be a function');
+
+      return middleware(prevText);
+    }, text);
+  }
 
   return {
-    getLocale() {
-      return currentLocale;
+    register(name, texts) {
+      // check that resources have not been already registered
+      if (l10nNames.indexOf(name) === -1) {
+        l10nNames.push(name);
+
+        // extend l10n texts
+        for (let key in texts) {
+          if (texts.hasOwnProperty(key)) {
+            l10nTexts[key] = texts[key];
+          }
+        }
+      }
     },
     getText(code) {
-      return texts[code] || code;
+      const text = l10nTexts[code];
+      return text ? applyMiddleware(text) : code;
     }
   };
 }
--- l10n-manager.test.js
+++ l10n-manager.test.js
@@ -1,13 +1,34 @@
 const LocalizationManager = require('./l10n-manager');
 
-
+// test data
 const l10nResources = {
   'button.open': 'Open',
   'button.close': 'Close',
 };
-const l10n = new LocalizationManager('en', l10nResources);
+
+// test middlewares
+function addZero(text) {
+  return text + '0';
+}
+
+function addOne(text) {
+  return text + '1';
+}
+
+
+// test execution
+const l10n = new LocalizationManager(addZero, addOne);
+l10n.register('test', l10nResources);
 
 
 console.log(l10n.getText('button.open'));
 console.log(l10n.getText('button.close'));
 console.log(l10n.getText('nonexistent.code'));
+
+
+console.time('getText-10000-times');
+let result = '';
+for (let i = 0; i < 10000; i++) {
+  result = l10n.getText('button.open');
+}
+console.timeEnd('getText-10000-times');
